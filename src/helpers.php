28ee92fd6d1290db3f9ddacbbd0c97bf19572c36
Carlos <carlos.rodriguez@techedgegroup.com>
2020-02-11 11:26:06 +0100

    add active route

--- src/helpers.php
+++ src/helpers.php
@@ -21,3 +21,52 @@ if (! function_exists('nt_route'))
         return route($name, $parameters, $absolute);
     }
 }
+
+if (! function_exists('active_route'))
+{
+    /**
+     * Get user country from session.
+     * @param   string      $routeNames         names of routes to check
+     * @param   string      $class              class to return if route is active
+     * @param   bool        $firstOccurrence    active to find first occurrence of route, this method is valid to active menu on subsections
+     * @return  boolean
+     */
+    function active_route($routeNames, $class = 'active', $firstOccurrence = false)
+    {
+        // if doesn't has any route with the current url, Request::route() will be null
+        if (Request::route() === null) return null;
+
+        if (!is_array($routeNames)) $routeNames = [$routeNames];
+
+        $found = false; // found occurrence
+
+        if ($firstOccurrence)
+        {
+            foreach ($routeNames as $routeName)
+            {
+                if (strpos(Request::url(), route($routeName)) !== 0)
+                {
+                    $found = true;
+                    break;
+                }
+            }
+        }
+        else
+        {
+            // check that route exist
+            if (Request::route() === null)
+                return false;
+
+            foreach ($routeNames as $routeName)
+            {
+                if (Request::route()->getName() === $routeName)
+                {
+                    $found = true;
+                    break;
+                }
+            }
+        }
+
+        if ($found) return $class;
+    }
+}

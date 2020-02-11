ad23dd0bab0be9aaf945e8e3c0433da63b75b6b6
Carlos <carlos.rodriguez@techedgegroup.com>
2020-02-11 03:07:12 +0100

    init commit

---
+++ src/Quasar/Navtools/NavtoolsServiceProvider.php
@@ -0,0 +1,40 @@
+<?php namespace Quasar\Navtools;
+
+use Illuminate\Support\ServiceProvider;
+
+class NavtoolsServiceProvider extends ServiceProvider
+{
+	/**
+	 * Bootstrap the application services.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+        // register migrations
+        $this->loadMigrationsFrom(__DIR__ . '/../../database/migrations');
+
+        // register translations
+        $this->loadTranslationsFrom(__DIR__ . '/../../resources/lang', 'navtools');
+
+        // register seeds
+        $this->publishes([
+            __DIR__ . '/../../database/seeds/' => base_path('/database/seeds')
+        ], 'seeds');
+
+        // register config
+        $this->publishes([
+            __DIR__ . '/../../config/quasar-navtools.php' => config_path('quasar-navtools.php')
+        ], 'config');
+	}
+
+	/**
+	 * Register the application services.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+        //
+	}
+}

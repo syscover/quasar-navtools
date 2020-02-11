6bfc3f19e31dfed87a585eeba533d5cced3e4b08
Carlos <carlos.rodriguez@techedgegroup.com>
2020-02-11 15:04:59 +0100

    wip

--- src/Quasar/Navtools/NavtoolsServiceProvider.php
+++ src/Quasar/Navtools/NavtoolsServiceProvider.php
@@ -11,21 +11,6 @@ class NavtoolsServiceProvider extends ServiceProvider
 	 */
 	public function boot()
 	{
-        /* // register migrations
-        $this->loadMigrationsFrom(__DIR__ . '/../../database/migrations');
-
-        // register translations
-        $this->loadTranslationsFrom(__DIR__ . '/../../resources/lang', 'navtools');
-
-        // register seeds
-        $this->publishes([
-            __DIR__ . '/../../database/seeds/' => base_path('/database/seeds')
-        ], 'seeds');
-
-        // register config
-        $this->publishes([
-            __DIR__ . '/../../config/quasar-navtools.php' => config_path('quasar-navtools.php')
-        ], 'config'); */
 	}
 
 	/**
